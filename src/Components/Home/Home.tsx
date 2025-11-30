0203e1ab1c2a0a4570d94254bf99b460ca03807e
aleksandre <aleksichokheli03@gmail.com>
2025-12-01 02:22:14 +0400

    finished half part of the home page

---
+++ src/Components/Home/Home.tsx
@@ -0,0 +1,28 @@
+import "./home.css";
+import "../../GlobalCss/fonts.css";
+import laptop from "../../assets/laptop.png";
+const Home = () => {
+  return (
+    <>
+      <section className="home__text__div">
+        <h1 className="home__h1 dela-gothic-one ">LAPSTORE</h1>
+        <ul className="home__ul">
+          <li className="home__li montserrat">
+            High Quality Laptops, <br />
+            wide selection
+          </li>
+          <li className="home__li montserrat">User Friendly Environement</li>
+          <li className="home__li montserrat">Efficent Sales</li>
+        </ul>
+      </section>
+
+      <section className="home__image__section">
+        <div className="laptop__image__div">
+          <img src={laptop} alt="laptop" className="laptop" />
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Home;
