947cb29051b849f0bca7cdbee67530a66006d4de
aleksandre <aleksichokheli03@gmail.com>
2025-12-02 14:56:41 +0400

    finished home page UI

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,14 +1,19 @@
 import "./home.css";
 import "../../GlobalCss/fonts.css";
 import laptop from "../../assets/laptop.png";
-import Form from "./Form/Form";
+import { useState } from "react";
 
 const Home = () => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const [zoomed, setZoomed] = useState(false);
+    const toggleZoom = () =>{
+      setZoomed(prev => !prev)
+    }
   
   return (
     <>
       <section className="home__text__div">
-        <h1 className="home__h1 dela-gothic-one ">LAPSTORE</h1>
+        <h1 className="home__h1 dela-gothic-one ">Own the Best</h1>
         <ul className="home__ul">
           <li className="home__li montserrat">
             High Quality Laptops, <br />
@@ -17,15 +22,24 @@ const Home = () => {
           <li className="home__li montserrat">User Friendly Environement</li>
           <li className="home__li montserrat">Efficent Sales</li>
         </ul>
+
+        <button className="checkout dela-gothic-one" onClick={toggleZoom}>S H O P</button>
       </section>
 
       <section className="home__image__section">
         <div className="laptop__image__div">
-          <img src={laptop} alt="laptop" className="laptop" />
+          <img src={laptop} alt="laptop" className="laptop"  />
         </div>
-        <Form/>
-      </section>
 
+        <div className="screen__content">
+          <header className="screen__header">
+            <button className="screen__login__btn screen__btn montserrat">Sign Up</button>
+            <button className="screen__sugnup__btn screen__btn montserrat">Log In</button>
+
+          </header>
+          <h1 className="screen__text organix">LAPSTORE</h1>
+        </div>
+      </section>
     </>
   );
 };
